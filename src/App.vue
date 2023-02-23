d4373985dd44b1d89b174b869eed8489783c7407
Vivi <vivichi@yile.com.tw>
2023-02-23 17:47:55 +0800

    [新增] 全局 sass

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,28 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <i class="gg-youtube"></i>
   </nav>
   <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  background: $blue
+  +desktop
+    background: #aaa
+    +set-flex(center, center)
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+nav
+  padding: 30px
+  a
+    font-weight: bold
+    color: #2c3e50
+    &.router-link-exact-active
+      color: #42b983
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "~@/assets/sass/variable.sass"
+          @import "~@/assets/sass/main.sass"
+          @import "~@/assets/sass/reset.sass"
+          @import "~@/assets/sass/icon.sass"
+        `,
+      },
+    },
+  },
 });
